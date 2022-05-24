991b73b72c5c3d27db9b75718353c8006c587dd5
EnNe <1412800185@qq.com>
2022-05-24 23:54:42 +0800

    [dev][led] 1完善led设备 2更改目录结构

---
+++ scr/dev/led/ev_led.c
@@ -0,0 +1,62 @@
+﻿#include "./dev/led/ev_led.h"
+
+
+EV_TYPE_FUN_DEF(ev_led_type,HELP)
+{
+    EV_TYPE_FUN_GET_ARG(HELP);
+
+    return 0;
+}
+
+
+EV_TYPE_FUN_DEF(ev_led_type,LINK)
+{
+    EV_TYPE_FUN_GET_ARG(LINK);
+
+    if(ev_dri_e_check(GPIO,arg->drive) == 0)
+    {
+        EV_DRI_S(GPIO) *gpio = arg->drive->p;
+    }
+
+    return 0;
+}
+
+EV_TYPE_FUN_DEF(ev_led_type,LED_SET)
+{
+    EV_TYPE_FUN_GET_ARG(LED_SET);
+    
+    arg->state = 0;
+    return 0;
+}
+
+
+EV_TYPE_FUN_DEF(ev_led_type,LED_TOGLE)
+{
+    EV_TYPE_FUN_GET_ARG(LED_TOGLE);
+
+    return 0;
+}
+
+static const edev_obj_fun_t EV_TYPE_LIST(ev_led_type)[] ={
+    EV_TYPE_LIST_ADD_FUN(ev_led_type,HELP),
+    EV_TYPE_LIST_ADD_FUN(ev_led_type,LINK),
+    EV_TYPE_LIST_ADD_FUN(ev_led_type,LED_SET),
+    EV_TYPE_LIST_ADD_FUN(ev_led_type,LED_TOGLE),
+};
+
+
+const ev_type_s ev_led_type = {
+    .name = "ev_led_type",
+    .vals_create = 0,
+    .vals_free = 0,
+
+    .total = EV_TYPE_TOTAL(ev_led_type),
+    .list  = EV_TYPE_LIST(ev_led_type),
+};
+
+
+
+
+
+
+
--- scr/dev/led/ev_led.h
+++ scr/dev/led/ev_led.h
@@ -9,19 +9,19 @@
 #include "./core/ev_core.h"
 
 enum{
-    EVD_LED_SET = EVO_STARE,
-    EVD_LED_TOGGLE,
+    EVO_E(LED_SET) = EVO_E(STARE),
+    EVO_E(LED_TOGLE),
 };
 
 //EVD_LED_SET
 typedef struct {
     uint8_t state;// 1为点亮 0为熄灭
-}EVO_S(EVD_LED_SET);
+}EVO_S(LED_SET);
 
 //EVD_LED_TOGGLE
 typedef struct {
     uint8_t no_arg;//无参
-}EVO_S(EVD_LED_TOGGLE);
+}EVO_S(LED_TOGLE);
 
 extern const ev_type_s ev_led_type;
 
