54c9ecfc06281147c71f47e2dd1f166949d4a8dd
EnNe <1412800185@qq.com>
2022-05-24 23:53:22 +0800

    [core] 1完善核心代码 2增加驱动方法定义

--- scr/core/ev_core.h
+++ scr/core/ev_core.h
@@ -2,7 +2,7 @@
 #define __EDEV_CORE_H__
 
 #include "stdint.h"
-
+#include "./dri/ev_dri.h"
 
 typedef struct ev_obj_s ev_obj_s;
 typedef struct ev_type_s ev_type_s;
@@ -24,15 +24,16 @@ typedef struct ev_type_s{
     uint16_t        total;      //方法总数
     const edev_obj_fun_t  *list;       //方法清单
 }ev_type_s;
+
 //定义list时的快捷方法
-#define EV_TYPE_LIST(type)                  type##_list
-#define EV_TYPE_TOTAL(type)                 sizeof(EV_TYPE_LIST(type))/sizeof(edev_obj_fun_t)
-#define EV_TYPE_FUN(type,EVO_E)             type##_##EVO_E
+#define EV_TYPE_LIST(type)              type##_list
+#define EV_TYPE_TOTAL(type)             sizeof(EV_TYPE_LIST(type))/sizeof(edev_obj_fun_t)
+#define EV_TYPE_FUN(type,OP)            type##_##OP
 //方法定义
-#define EV_TYPE_FUN_DEF(type,EVO_E)         static uint8_t EV_TYPE_FUN(type,EVO_E)(ev_obj_s *self,void *_arg)
+#define EV_TYPE_FUN_DEF(type,OP)        static uint8_t EV_TYPE_FUN(type,OP)(ev_obj_s *self,void *_arg)
 //获得参数到 arg变量
-#define EV_TYPE_FUN_GET_ARG(EVO_E)           EVO_S(EVO_E) *arg = _arg
-#define EV_TYPE_LIST_ADD_FUN(type,EVO_E)    [EVO_E] = EV_TYPE_FUN(type,EVO_E)
+#define EV_TYPE_FUN_GET_ARG(OP)          EVO_S(OP) *arg = _arg
+#define EV_TYPE_LIST_ADD_FUN(type,OP)    [EVO_E(OP)] = EV_TYPE_FUN(type,OP)
 
 
 //设备对象结构
@@ -60,24 +61,25 @@ if(op >= obj->type->total)\
 
 //--------------------------------
 //选项edev Options -> evo
-#define EVO_S(EVO_E) EVO_E##_ARG_S
+#define EVO_E(OP) EVO_##OP##_E
+#define EVO_S(OP) EVO_##OP##_ARG_S
 extern uint8_t _ev_obj_fun(ev_obj_s *obj, uint16_t op, void *arg);
 
 //基础选项
 
 //基础选项列表,每一个设备都需要有的选项
 enum{
-    EVO_HELP = 0,    //帮助信息,可重定义
-    EVO_POWER,       //默认电源配置,可重定义
-    EVO_LINK,        //默认连接下级设备,可重定义
-    EVO_SET_CB,      //默认设置回调,可重定义
-    EVO_STARE,       //设备方法的开始
+    EVO_E(HELP) = 0,    //帮助信息,可重定义
+    EVO_E(POWER),       //默认电源配置,可重定义
+    EVO_E(LINK),        //默认连接下级设备,可重定义
+    EVO_E(SET_CB),      //默认设置回调,可重定义
+    EVO_E(STARE),       //设备方法的开始
 };//最多支持65535个，没必要考虑更多的情况
 
 //EVO_HELP 帮助信息 注释或打印
 typedef struct {
     uint16_t op;
-}EVO_S(EVO_HELP);
+}EVO_S(HELP);
 
 //--------------------------------
 //电源等级edev power -> evp
@@ -91,14 +93,14 @@ enum{
 //EVO_LINK 默认电源配置,可重定义
 typedef struct {
     uint8_t    evp;//电源等级
-}EVO_S(EVO_POWER);
+}EVO_S(POWER);
 
 
 //EVO_LINK 默认的连接下级设备
 typedef struct {
     ev_obj_s    *obj;//嵌套其他设备 W25WXX
-    void        *drive;//芯片驱动 GPIO,SPI
-}EVO_S(EVO_LINK);
+    ev_dri_t    *drive;//芯片驱动 GPIO,SPI
+}EVO_S(LINK);
 
 
 //EVO_SET_CB 默认的设置回调
@@ -106,7 +108,7 @@ typedef void (*ev_obj_cb_t)(ev_obj_s *obj,uint16_t event,void *arg);
 
 typedef struct {
     ev_obj_cb_t cb;
-}EVO_S(EVO_SET_CB);
+}EVO_S(SET_CB);
 
 
 //--------------------------------
---
+++ scr/dri/ev_dri.h
@@ -0,0 +1,47 @@
+﻿#ifndef __EDEV_DRI_H__
+#define __EDEV_DRI_H__
+
+typedef const void * ev_drive_s;
+
+#define EV_DRI_E(DRI) EV_DRI_##DRI##_E
+#define EV_DRI_S(DRI) const EV_DRI_##DRI##_S
+  
+
+#define ev_drive_forge(DRI, ...)  &(const EV_DRI_S(DRI)[]){{__VA_ARGS__}}
+
+enum{
+    EV_DRI_E(BASE) = 1,
+    EV_DRI_E(GPIO),
+    EV_DRI_E(SPI),
+    EV_DRI_E(IIC),
+};
+
+typedef uint16_t ev_dri_e;
+
+typedef struct{
+    ev_dri_e    e;//枚举标识符
+    ev_drive_s  p;
+}ev_dri_t;
+
+//判断类型-错误返回1
+#define ev_dri_e_check(DRI_E,dri) \
+((dri)->e != EV_DRI_E(DRI_E))
+
+
+typedef struct{
+    uint8_t   no_arg;
+}EV_DRI_S(BASE);
+
+typedef struct{
+    //设置电平
+    void       (*set)(uint8_t val);
+    //反转电平
+    void       (*togle)(void);
+}EV_DRI_S(GPIO);
+
+typedef struct{
+    uint8_t   no_arg;
+}EV_DRI_S(SPI);
+
+
+#endif
