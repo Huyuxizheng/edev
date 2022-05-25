4346bed097998d8ef5dcc102906cc3f17b7968a7
EnNe <1412800185@qq.com>
2022-05-25 21:20:16 +0800

    [dev][led] 1.完善LED设备

--- scr/dev/led/ev_led.c
+++ scr/dev/led/ev_led.c
@@ -1,9 +1,37 @@
 ﻿#include "./dev/led/ev_led.h"
+#include "edev_config.h"
 
+typedef struct{
+    EV_DRI_S(GPIO) *gpio;
+    uint8_t         en_val;
+}ev_led_vals_s;
 
 EV_TYPE_FUN_DEF(ev_led_type,HELP)
 {
     EV_TYPE_FUN_GET_ARG(HELP);
+/*
+    //创建led 的 gpio驱动
+    ev_drive_s *led_gpio = ev_drive_forge(GPIO, 
+                                            .group = GPIOA,
+                                            .pin   = PIN_1,
+                                            .set   = gpio_set,
+                                            .togle = gpio_togle
+                                        );
+    //创建LED设备
+    ev_obj_s *led_dev = ev_obj_create(ev_led_type);
+
+    //LED设备增加gpio驱动
+    ev_obj_fun(led_dev, LINK, led_gpio);
+
+    //设置LED引脚高电平为点亮
+    ev_obj_fun(led_dev, LED_SET_EN_VAL, 1);  
+
+    //设置LED
+    ev_obj_fun(led_dev, LED_SET, 1);  
+
+    //反转LED
+    ev_obj_fun(led_dev, LED_TOGLE, 0);                                       
+*/
 
     return 0;
 }
@@ -13,19 +41,25 @@ EV_TYPE_FUN_DEF(ev_led_type,LINK)
 {
     EV_TYPE_FUN_GET_ARG(LINK);
 
+    ev_led_vals_s *vals = self->vals;
+
     if(ev_dri_e_check(GPIO,arg->drive) == 0)
     {
-        EV_DRI_S(GPIO) *gpio = arg->drive->p;
+        vals->gpio = arg->drive->p;
+        return 0;
     }
 
-    return 0;
+    return 1;
 }
 
 EV_TYPE_FUN_DEF(ev_led_type,LED_SET)
 {
     EV_TYPE_FUN_GET_ARG(LED_SET);
-    
-    arg->state = 0;
+    ev_led_vals_s *vals = self->vals;
+    if(vals->gpio)
+    {
+        ev_dri_gpio_set(vals->gpio,vals->en_val);
+    }
     return 0;
 }
 
@@ -33,6 +67,20 @@ EV_TYPE_FUN_DEF(ev_led_type,LED_SET)
 EV_TYPE_FUN_DEF(ev_led_type,LED_TOGLE)
 {
     EV_TYPE_FUN_GET_ARG(LED_TOGLE);
+    ev_led_vals_s *vals = self->vals;
+    if(vals->gpio)
+    {
+        ev_dri_gpio_togle(vals->gpio);
+    }
+
+    return 0;
+}
+EV_TYPE_FUN_DEF(ev_led_type,LED_SET_EN_VAL)
+{
+    EV_TYPE_FUN_GET_ARG(LED_SET_EN_VAL);
+    ev_led_vals_s *vals = self->vals;
+
+    vals->en_val = arg->en_val;
 
     return 0;
 }
@@ -43,20 +91,35 @@ static const edev_obj_fun_t EV_TYPE_LIST(ev_led_type)[] ={
     EV_TYPE_LIST_ADD_FUN(ev_led_type,LED_SET),
     EV_TYPE_LIST_ADD_FUN(ev_led_type,LED_TOGLE),
 };
-
+uint8_t ev_led_type_vals_create(ev_obj_s *obj)
+{
+    obj->vals = ev_malloc(sizeof(ev_led_vals_s));
+    if(obj->vals)
+    {
+        ((ev_led_vals_s *)obj->vals)->en_val = 0;
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+//自变量释放方法
+void ev_led_type_vals_free(ev_obj_s *obj)
+{
+    if(obj->vals)
+    {
+        ev_free(obj->vals);
+    }
+}
 
 const ev_type_s ev_led_type = {
     .name = "ev_led_type",
-    .vals_create = 0,
-    .vals_free = 0,
+    .vals_create = ev_led_type_vals_create,
+    .vals_free = ev_led_type_vals_free,
 
     .total = EV_TYPE_TOTAL(ev_led_type),
     .list  = EV_TYPE_LIST(ev_led_type),
 };
-
-
-
-
-
 
 
--- scr/dev/led/ev_led.h
+++ scr/dev/led/ev_led.h
@@ -1,5 +1,5 @@
-﻿#ifndef __EDEV_CONFIG_H__
-#define __EDEV_CONFIG_H__
+﻿#ifndef __EDEV_LED_H__
+#define __EDEV_LED_H__
 
 /*
 一般的LED操作，可设置点亮，或反转电平
@@ -11,6 +11,7 @@
 enum{
     EVO_E(LED_SET) = EVO_E(STARE),
     EVO_E(LED_TOGLE),
+    EVO_E(LED_SET_EN_VAL),
 };
 
 //EVD_LED_SET
@@ -22,6 +23,11 @@ typedef struct {
 typedef struct {
     uint8_t no_arg;//无参
 }EVO_S(LED_TOGLE);
+
+//EVD_LED_TOGGLE
+typedef struct {
+    uint8_t en_val;//设置点亮的电平值
+}EVO_S(LED_SET_EN_VAL);
 
 extern const ev_type_s ev_led_type;
 
