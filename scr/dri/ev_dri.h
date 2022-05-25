1fec57c6ba618d9062030db494168a2d9722d571
EnNe <1412800185@qq.com>
2022-05-25 21:19:46 +0800

    [dri][gpio] 1.完善GPIO驱动

--- scr/dri/ev_dri.h
+++ scr/dri/ev_dri.h
@@ -1,13 +1,15 @@
 ﻿#ifndef __EDEV_DRI_H__
 #define __EDEV_DRI_H__
 
-typedef const void * ev_drive_s;
+typedef const void * ev_dri_type;
 
 #define EV_DRI_E(DRI) EV_DRI_##DRI##_E
 #define EV_DRI_S(DRI) const EV_DRI_##DRI##_S
   
+#define EV_DRI_DEF(DRI, ...)  &(const EV_DRI_S(DRI)[]){{__VA_ARGS__}}
 
-#define ev_drive_forge(DRI, ...)  &(const EV_DRI_S(DRI)[]){{__VA_ARGS__}}
+#define _ev_drive_forge(DRI, ...)  (ev_drive_s *)&(const ev_drive_s []){{.e = EV_DRI_E(DRI),.p = EV_DRI_DEF(DRI, __VA_ARGS__)}}  
+
 
 enum{
     EV_DRI_E(BASE) = 1,
@@ -19,9 +21,9 @@ enum{
 typedef uint16_t ev_dri_e;
 
 typedef struct{
-    ev_dri_e    e;//枚举标识符
-    ev_drive_s  p;
-}ev_dri_t;
+    ev_dri_e       e;//枚举标识符
+    ev_dri_type    p;
+}ev_drive_s;
 
 //判断类型-错误返回1
 #define ev_dri_e_check(DRI_E,dri) \
@@ -32,12 +34,8 @@ typedef struct{
     uint8_t   no_arg;
 }EV_DRI_S(BASE);
 
-typedef struct{
-    //设置电平
-    void       (*set)(uint8_t val);
-    //反转电平
-    void       (*togle)(void);
-}EV_DRI_S(GPIO);
+
+#include "./dri/ev_dri_gpio.h"
 
 typedef struct{
     uint8_t   no_arg;
---
+++ scr/dri/ev_dri_gpio.h
@@ -0,0 +1,32 @@
+﻿#ifndef __EDEV_DRI_GPIO_H__
+#define __EDEV_DRI_GPIO_H__
+#include "./dri/ev_dri.h"
+
+
+typedef struct{
+    //可选的 GPIO组
+    uint32_t    group;
+    //可选的 GPIO pin脚编号
+    uint32_t    pin;
+    //设置电平
+    void        (*set)(uint32_t group,uint32_t pin,uint8_t val);
+    //获得电平
+    uint8_t     (*get)(uint32_t group,uint32_t pin);
+    //反转电平
+    void        (*togle)(uint32_t group,uint32_t pin);
+}EV_DRI_S(GPIO);
+inline void ev_dri_gpio_set(EV_DRI_S(GPIO) *gpio,uint8_t val){
+    if(gpio->set)
+        gpio->set(gpio->group,gpio->pin,val);
+}
+inline uint8_t ev_dri_gpio_get(EV_DRI_S(GPIO) *gpio){
+    if(gpio->get)
+        return gpio->get(gpio->group,gpio->pin);
+    return 0;
+}
+inline void ev_dri_gpio_togle(EV_DRI_S(GPIO) *gpio){
+    if(gpio->togle)
+        gpio->togle(gpio->group,gpio->pin);
+}
+
+#endif
