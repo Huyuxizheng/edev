a6eb8cb775a5ee93adffa4ee0b7eb975313bf2e0
EnNe <1412800185@qq.com>
2022-05-26 22:39:22 +0800

    [dev][tp] 1增加tp设备

---
+++ scr/dev/tp/ev_tp.c
@@ -0,0 +1,102 @@
+﻿#include "./dev/led/ev_tp.h"
+#include "edev_config.h"
+
+#include "./dev/led/ev_led.h"
+#include "edev_config.h"
+
+typedef struct ev_obj_list_s ev_obj_list_s;
+
+typedef struct ev_obj_list_s{
+    ev_obj_list_s  *next;
+    ev_obj_s       *en_val;
+}ev_obj_list_s;
+
+typedef struct{
+    ev_obj_s        *obj;
+    ev_obj_list_s   *obj_list;
+}ev_tp_vals_s;
+
+EV_TYPE_FUN_DEF(ev_tp_type,HELP)
+{
+    EV_TYPE_FUN_GET_ARG(HELP);
+/*
+                                   
+*/
+
+    return 0;
+}
+
+
+EV_TYPE_FUN_DEF(ev_tp_type,LINK)
+{
+    EV_TYPE_FUN_GET_ARG(LINK);
+
+    ev_led_vals_s *vals = self->vals;
+
+
+    return 1;
+}
+
+EV_TYPE_FUN_DEF(ev_tp_type,POWER)
+{
+    EV_TYPE_FUN_GET_ARG(POWER);
+
+    ev_led_vals_s *vals = self->vals;
+
+
+    return 0;
+}
+EV_TYPE_FUN_DEF(ev_tp_type,TP_GET)
+{
+    EV_TYPE_FUN_GET_ARG(TP_GET);
+    ev_led_vals_s *vals = self->vals;
+
+    return 0;
+}
+
+
+
+
+static const edev_obj_fun_t EV_TYPE_LIST(ev_tp_type)[] ={
+    EV_TYPE_LIST_ADD_FUN(ev_tp_type,HELP),
+    EV_TYPE_LIST_ADD_FUN(ev_tp_type,LINK),
+    EV_TYPE_LIST_ADD_FUN(ev_tp_type,POWER),
+    EV_TYPE_LIST_ADD_FUN(ev_tp_type,TP_GET),
+};
+uint8_t ev_tp_type_vals_create(ev_obj_s *obj)
+{
+    obj->vals = ev_malloc(sizeof(ev_tp_vals_s));
+    if(obj->vals)
+    {
+        ((ev_tp_vals_s *)obj->vals)->obj = 0;
+        ((ev_tp_vals_s *)obj->vals)->obj_list = 0;
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+//自变量释放方法
+void ev_tp_type_vals_free(ev_obj_s *obj)
+{
+    if(obj->vals)
+    {
+        ev_free(obj->vals);
+    }
+}
+
+const ev_type_s ev_tp_type = {
+    .name = "ev_tp_type",
+    .vals_create = ev_tp_type_vals_create,
+    .vals_free = ev_tp_type_vals_free,
+
+    .total = EV_TYPE_TOTAL(ev_tp_type),
+    .list  = EV_TYPE_LIST(ev_tp_type),
+};
+
+
+
+
+
+
---
+++ scr/dev/tp/ev_tp.h
@@ -0,0 +1,34 @@
+﻿#ifndef __EDEV_TP_H__
+#define __EDEV_TP_H__
+
+/*
+触摸驱动
+*/
+
+
+#include "./core/ev_core.h"
+
+enum{
+    EVO_E(TP_GET) = EVO_E(STARE),
+};
+
+//EVD_TP_GET
+typedef struct{
+    uint16_t x;
+    uint16_t y;
+    uint32_t tick;
+    uint8_t  statie;
+}EVO_S(TP_GET_OUT);
+
+typedef struct {
+    EVO_S(TP_GET_OUT) *out;
+}EVO_S(TP_GET);
+
+
+extern const ev_type_s ev_dri_type;
+
+
+
+
+
+#endif
