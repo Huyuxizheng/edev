ff392c75bd47f2f66a57161e01bb617488c23b62
huyu <1412800185@qq.com>
2022-05-26 15:02:29 +0800

    [core] 1增加锁机制

--- scr/core/ev_core.c
+++ scr/core/ev_core.c
@@ -1,5 +1,6 @@
 ﻿#include "edev.h"
-
+static void* ev_global_lock = 0;
+static uint8_t ev_absolute_global_lock_en = 0;
 void ev_obj_free(ev_obj_s *obj)
 {
     if(!obj)
@@ -63,12 +64,91 @@ uint8_t _ev_obj_fun(ev_obj_s *obj, uint16_t op, void *arg)
 
     if(obj->type->list[op])
     {
-        return obj->type->list[op](obj, arg);
+        if(!ev_absolute_global_lock_en)
+        {
+            if(obj->lock)
+            {
+                ev_os_lock(obj->lock);
+            }
+            uint8_t ret = obj->type->list[op](obj, arg);
+            if(obj->lock)
+            {
+                ev_os_unlock(obj->lock);
+            }
+            return ret;
+        }
+        else
+        {
+            if(!ev_global_lock)
+            {
+                ev_global_lock = ev_os_lock_create();
+            }
+            if(ev_global_lock)
+            {
+                ev_os_lock(ev_global_lock);
+            }
+            uint8_t ret = obj->type->list[op](obj, arg);
+            if(ev_global_lock)
+            {
+                ev_os_unlock(ev_global_lock);
+            }
+            return ret;
+        }
     }
     return 0;
 }
+void ev_obj_fun_absolute_security_en(uint8_t en)
+{
+    ev_absolute_global_lock_en  = en;
+}
+uint8_t ev_obj_fun_obj_security_en(ev_obj_s *obj,uint8_t en)
+{
+    if(en)
+    {
+        if(!obj->lock)
+        {
+            obj->lock =  ev_os_lock_create();
+            if(!obj->lock)
+            {
+                return 1;
+            }
+        }
+    }
+    else
+    {
+        if(obj->lock)
+        {
+            ev_os_lock_free(obj->lock);
+            obj->lock = 0;
+        }
+    }
+    return 0;
+}
+uint8_t _ev_obj_fun_security(ev_obj_s *obj, uint16_t op, void *arg)
+{
+    ev_obj_assert(obj)
+    ev_type_assert(obj)
+    ev_op_assert(obj,op)
 
-
+    if(obj->type->list[op])
+    {
+        if(!ev_global_lock)
+        {
+            ev_global_lock = ev_os_lock_create();
+        }
+        if(ev_global_lock)
+        {
+            ev_os_lock(ev_global_lock);
+        }
+        uint8_t ret = obj->type->list[op](obj, arg);
+        if(ev_global_lock)
+        {
+            ev_os_unlock(ev_global_lock);
+        }
+        return ret;
+    }
+    return 0;
+}
 
 
 
--- scr/edev_config.h
+++ scr/edev_config.h
@@ -5,6 +5,11 @@
 #define ev_malloc(size) malloc(size)
 #define ev_free(p)      free(p)  
 
+//递归互斥锁
+#define ev_os_lock_create()           malloc(1)
+#define ev_os_lock_free(lock_p)      free(lock_p)  
+#define ev_os_lock(lock_p)    
+#define ev_os_unlock(lock_p)
 
 #define ev_error(...)  
 #define ev_warning(...)        
