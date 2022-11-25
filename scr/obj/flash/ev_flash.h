4655401f9183580576e1617577c896bea4d47b14
huyu <1412800185@qq.com>
2022-11-25 17:55:53 +0800

    增加flash模型

---
+++ scr/obj/flash/ev_flash.h
@@ -0,0 +1,46 @@
+#ifndef __EDEV_FLASH_H__
+#define __EDEV_FLASH_H__
+
+
+#include "./core/ev_core.h"
+
+
+enum{
+    EVO_E(FLASH_WRITE) = EVO_E(START),
+    EVO_E(FLASH_READ),
+    EVO_E(FLASH_ERASE),
+};
+
+
+EV_FUN_DEF(FLASH_INIT);
+
+
+EV_FUN_DEF(FLASH_WRITE,uint32_t offset,const uint8_t *data,uint32_t size);
+
+EV_FUN_DEF(FLASH_READ,uint32_t offset,uint8_t *data,uint32_t size);
+
+//擦除指定数量块/扇区
+EV_FUN_DEF(FLASH_ERASE,uint32_t offset,uint32_t num);
+
+
+
+extern const ev_model_t ev_flash_m;
+
+typedef struct{//属性列表
+    ev_obj_attr_base_t  base;//固定头
+
+    uint8_t     (*init)(void);
+
+    uint8_t     (*write)(uint32_t offset,const uint8_t *data,uint32_t size);
+
+    uint8_t     (*read)(uint32_t offset,const uint8_t *data,uint32_t size);
+
+    uint8_t     (*erase)(uint32_t offset,uint32_t num);
+
+    uint8_t     (*uninit)(void);
+
+}EVO_ATTR_T(ev_flash_m);
+#define ev_flash_m_attr_init  
+
+#endif
+
