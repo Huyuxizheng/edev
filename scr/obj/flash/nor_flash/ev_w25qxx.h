f5d7e97c9752e5395cae54594200b79d8ad6341d
EnNe <1412800185@qq.com>
2022-11-27 20:40:58 +0800

    增加w25qxx模型

---
+++ scr/obj/flash/nor_flash/ev_w25qxx.h
@@ -0,0 +1,31 @@
+﻿#ifndef __EDEV_W25QXX_H__
+#define __EDEV_W25QXX_H__
+
+
+
+#include "./core/ev_core.h"
+
+
+
+typedef struct EVO_ATTR_T(ev_w25qxx_spi_m){//属性列表
+    ev_obj_attr_base_t  base;//固定头
+    const ev_obj_t      *spi;
+    const ev_obj_t      *cs;
+}EVO_ATTR_T(ev_w25qxx_spi_m);
+#define ev_w25qxx_spi_m_attr_init 
+
+extern const ev_model_t ev_w25qxx_spi_m;
+
+
+
+typedef struct{//属性列表
+    EVO_ATTR_T(ev_w25qxx_spi_m) ;
+}EVO_ATTR_T(ev_w25q32_spi_m);
+#define ev_w25q32_spi_m_attr_init  ev_w25qxx_spi_m_attr_init
+
+extern const ev_model_t ev_w25q32_spi_m;
+
+
+
+
+#endif
---
+++ scr/obj/flash/nor_flash/ev_w25qxx_spi.c
@@ -0,0 +1,67 @@
+﻿#include "./core/ev_core.h"
+#include "./obj/flash/ev_flash.h"
+#include "./obj/flash/nor_flash/ev_w25qxx.h"
+#include "edev_config.h"
+
+
+EV_MODEL_FUN_DEF(ev_w25qxx_spi_m,HELP)
+{
+    EV_MODEL_FUN_GET_ARG(ev_w25qxx_spi_m,HELP);
+
+
+    return 0;
+}
+
+
+EV_MODEL_FUN_DEF(ev_w25qxx_spi_m,INIT)
+{
+    EV_MODEL_FUN_GET_ARG(ev_w25qxx_spi_m,INIT);
+
+
+    return 0;
+}
+
+EV_MODEL_FUN_DEF(ev_w25qxx_spi_m,FLASH_INFO)
+{
+    EV_MODEL_FUN_GET_ARG(ev_w25qxx_spi_m,FLASH_INFO);
+
+
+
+    return 0;
+}
+
+EV_MODEL_FUN_DEF(ev_w25qxx_spi_m,FLASH_WRITE)
+{
+    EV_MODEL_FUN_GET_ARG(ev_w25qxx_spi_m,FLASH_WRITE);
+
+
+    return 0;
+}
+
+EV_MODEL_FUN_DEF(ev_w25qxx_spi_m,FLASH_READ)
+{
+    EV_MODEL_FUN_GET_ARG(ev_w25qxx_spi_m,FLASH_READ);
+
+
+    return 0;
+}
+
+EV_MODEL_FUN_DEF(ev_w25qxx_spi_m,FLASH_ERASE)
+{
+    EV_MODEL_FUN_GET_ARG(ev_w25qxx_spi_m,FLASH_ERASE);
+
+
+    return 0;
+}
+
+EV_MODEL_FUN_DEF(ev_w25qxx_spi_m,UNINIT)
+{
+    EV_MODEL_FUN_GET_ARG(ev_w25qxx_spi_m,UNINIT);
+
+
+    return 0;
+}
+EV_MODEL_LIST_DEF(ev_w25qxx_spi_m,HELP,INIT,FLASH_WRITE,FLASH_READ,FLASH_ERASE,UNINIT);
+
+const ev_model_t ev_w25qxx_spi_m = EV_MODEL_DEF(ev_w25qxx_spi_m);
+
