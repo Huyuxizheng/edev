5cfb27e47ca351d513330766cbfb8771718c4bd7
EnNe <1412800185@qq.com>
2022-12-11 13:32:13 +0800

    1.修复w25qxx的BUG 2.完善w25nxx

--- scr/obj/flash/ev_nand.h
+++ scr/obj/flash/ev_nand.h
@@ -10,6 +10,7 @@ enum{
     EVO_E(NAND_READ),
     EVO_E(NAND_ERASE),
     EVO_E(NAND_INFO),
+    EVO_E(NAND_CHECK_BAD_BLOCK),
     EVO_E(NAND_SET_BAD_BLOCK),
 };
 
@@ -32,42 +33,33 @@ EV_FUN_DEF(NAND_READ,uint32_t page_offset,uint8_t *data,uint32_t size,uint8_t *o
 EV_FUN_DEF(NAND_ERASE,uint32_t page_offset);
 
 //设置坏块
+EV_FUN_DEF(NAND_CHECK_BAD_BLOCK,uint32_t page_offse,);
 EV_FUN_DEF(NAND_SET_BAD_BLOCK,uint32_t page_offset);
 
 
 
 enum{
-    EVO_E(NAND_ECC_WRITE) = EVO_E(START),
-    EVO_E(NAND_ECC_READ),
-    EVO_E(NAND_ECC_ERASE),
-    EVO_E(NAND_ECC_INFO),
-    EVO_E(NAND_ECC_OOB_READ),
-    EVO_E(NAND_ECC_OOB_WRITE),
+    EVO_E(NAND_ECC_INFO) = EVO_E(NAND_SET_BAD_BLOCK)+1,
+    EVO_E(NAND_ECC_NOOB_READ),
+    EVO_E(NAND_ECC_NOOB_WRITE),
 };
 
 
 typedef struct {
     uint32_t page_gran;     //页大小
     uint32_t block_size;    //块大小
-    uint32_t ecc_oob_size;  //可用被ECC保护的oob大小
     uint32_t oob_size;      //可用oob大小
     uint32_t block_num;     //块数量
+    uint32_t noob_size;     //不被ECC保护的oob大小
 } ev_nand_ecc_info_t;
 
 EV_FUN_DEF(NAND_ECC_INFO,ev_nand_ecc_info_t *info);
 
-//受ECC保护的数据和oob读写，受保护的数据只能一起写
-EV_FUN_DEF(NAND_ECC_WRITE,uint32_t page_offset,const uint8_t *data,uint32_t size,const uint8_t *oob_data,uint32_t oob_size);
-
-EV_FUN_DEF(NAND_ECC_READ,uint32_t page_offset,uint8_t *data,uint32_t size,uint8_t *oob_data,uint32_t oob_size);
-
-//擦除指定块
-EV_FUN_DEF(NAND_ECC_ERASE,uint32_t page_offset);
-
 //不受ECC保护的 OOB读写
-EV_FUN_DEF(NAND_ECC_OOB_WRITE,uint32_t page_offset,const uint8_t *oob_data,uint32_t oob_size);
+EV_FUN_DEF(NAND_ECC_NOOB_WRITE,uint32_t page_offset,const uint8_t *oob_data,uint32_t oob_size);
 
-EV_FUN_DEF(NAND_ECC_OOB_READ,uint32_t page_offset,uint8_t *oob_data,uint32_t oob_size);
+EV_FUN_DEF(NAND_ECC_NOOB_READ,uint32_t page_offset,uint8_t *oob_data,uint32_t oob_size);
+
 
 #endif
 
